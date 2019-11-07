d8fa47bebf7c9d000891617fa53bc9d849ebad0f
Laurens <laurensmonstrey@gmail.com>
2019-11-07 17:31:52 +0100

    create pokemon profile table

---
+++ app/Models/PokemonProfile.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PokemonProfile extends Model
+{
+      /**
+     * Set the table name
+     *
+     * @var string
+     */
+    protected $table = "pokemon_profiles";
+
+    /**
+     * Set the types of the attributes
+     *
+     * @var array
+     */
+    protected $casts = [
+       "pokemon_id" => "integer",
+       "abilities" => "json",
+       "base_experience" => "integer",
+       "forms" => "json",
+       "height" => "integer",
+       "held_items" => "json",
+       "is_default" => "boolean",
+       "location_area_encounters" => "string",
+       "moves" => "json",
+       "species" => "json",
+       "sprites" => "json",
+       "stats" => "json",
+       "types" => "json",
+       "weight" => "integer"
+    ];
+
+    /**
+     * Set the additional timestamp dates
+     *
+     * @var array
+     */
+    protected $dates = [
+      
+    ];
+
+    /**
+     * Set hidden attributes
+     *
+     * @var array
+     */
+    protected $hidden = [
+      
+    ];
+
+    /**
+     * The attributes that are mass guarded.
+     *
+     * @var array
+     */
+    protected $guarded = [
+
+    ];
+}
---
+++ database/migrations/2019_11_07_162549_create_pokemon_profiles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePokemonProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pokemon_profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pokemon_id')->unique()->index();
+            $table->json('abilities');
+            $table->integer('base_experience');
+            $table->json('forms');
+            $table->integer('height');
+            $table->json('held_items');
+            $table->boolean('is_default');
+            $table->string('location_area_encounters');
+            $table->json('moves');
+            $table->json('species');
+            $table->json('sprites');
+            $table->json('stats');
+            $table->json('types');
+            $table->integer('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pokemon_profiles');
+    }
+}
